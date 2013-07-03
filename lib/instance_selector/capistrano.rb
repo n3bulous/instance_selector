5edab7bf7c8252cb59ec7710e2f8c23bd5942f2b
Kevin McFadden <kmcfadden@gmail.com>
2013-07-02 21:05:41 -0600

    swap :aws for provider param, again kudos to CraigWilliams

--- lib/instance_selector/capistrano.rb
+++ lib/instance_selector/capistrano.rb
@@ -2,7 +2,7 @@ require 'instance_selector'
 
 Capistrano::Configuration.instance(:must_exist).load do
   def instance_selector(cap_role, provider, args={})
-    client = InstanceSelector::Connection.factory(:aws)
+    client = InstanceSelector::Connection.factory(provider)
     instances = client.instances(client.args_to_filters(args))
     instances.keys.each { |instance| role(cap_role, *instances.keys) }
   end
