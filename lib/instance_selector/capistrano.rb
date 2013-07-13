c26377525393be1dbe2b2041004f8130dc421c31
Kevin McFadden <kmcfadden@gmail.com>
2013-07-12 20:56:31 -0600

    instance_selector_list task lists a stage's instances

--- lib/instance_selector/capistrano.rb
+++ lib/instance_selector/capistrano.rb
@@ -5,5 +5,15 @@ Capistrano::Configuration.instance(:must_exist).load do
     client = InstanceSelector::Connection.factory(provider)
     instances = client.instances(client.args_to_filters(args))
     instances.keys.each { |instance| role(cap_role, *instances.keys) }
+    _cset(:instance_selector_instances) { instances }
   end
+
+  # Not namespaced due to collision with the above method.
+  desc "List all cloud instances for a stage"
+  task :instance_selector_list do
+    fetch(:instance_selector_instances).each do |instance|
+      puts instance.join("\t")
+    end
+  end
+
 end
