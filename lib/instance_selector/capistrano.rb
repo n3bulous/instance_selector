96a51294c5ed5c32f7d868cebfa92d00ff9f0d6d
Kevin McFadden <kmcfadden@gmail.com>
2013-07-12 21:25:24 -0600

    fix bug where listing only shows last role retrieved

--- lib/instance_selector/capistrano.rb
+++ lib/instance_selector/capistrano.rb
@@ -1,19 +1,23 @@
 require 'instance_selector'
 
 Capistrano::Configuration.instance(:must_exist).load do
+  # Yes, this is a hack
+  @instance_selector_instances = {}
+
   def instance_selector(cap_role, provider, args={})
     client = InstanceSelector::Connection.factory(provider)
     instances = client.instances(client.args_to_filters(args))
-    instances.keys.each { |instance| role(cap_role, *instances.keys) }
-    _cset(:instance_selector_instances) { instances }
+    role(cap_role, *instances.keys)
+
+    @instance_selector_instances.merge!(instances)
   end
 
   # Not namespaced due to collision with the above method.
   desc "List all cloud instances for a stage"
   task :instance_selector_list do
-    fetch(:instance_selector_instances).each do |instance|
+    puts
+    @instance_selector_instances.sort_by {|k,v| v}.each do |instance|
       puts instance.join("\t")
     end
   end
-
 end
